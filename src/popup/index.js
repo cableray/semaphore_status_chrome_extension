43e4e47266b81d3ac2cfc65885645de58322d848
Garrett Motzner <garrett.motzner@welltok.com>
2016-01-29 16:03:53 -0700

    Show server status in popup

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,4 +1,39 @@
-import welcome from 'shared/welcome'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import http from 'axios'
+import {map} from 'lodash'
 import "shared/page.css"
+import Server from './serverCompn'
 
-welcome('popup/index.js')
+class ServerStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayName = 'ServerStatus'
+    this.state = {semaphoreResponse: {}}
+  }
+
+  componentDidMount () {
+    const {auth_hash_id, auth_key} = this.props.options
+    http.get(`https://semaphoreci.com/api/v1/projects/${auth_hash_id}/servers`, {params:{auth_token: auth_key}})
+      .then((response) => { this.setState({semaphoreResponse: response.data}) })
+      .catch((response) => console.error('semaphore server error:', response))
+  }
+
+  render() {
+    return (
+      <ul>
+        {map(this.state.semaphoreResponse, (response) => (
+          <Server key={response.id} data={response} />
+          ))}
+      </ul>
+      )
+  }
+}
+
+chrome.storage.sync.get(null, options => {
+  console.log('rendering with', options)
+  ReactDOM.render(
+    <ServerStatus options={options} />,
+    document.getElementById('status')
+  )
+})
---
+++ src/popup/serverCompn.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import http from 'axios'
+
+class Server extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayName = 'ServerStatusDetail'
+    this.state = {semaphoreResponse: {commit:{}}}
+  }
+
+  componentDidMount () {
+    http.get(this.props.data.server_url)
+      .then((response) => { this.setState({semaphoreResponse: response.data}) })
+      .catch((response) => console.error('semaphore server error:', response))
+  }
+
+  render() {
+    const {
+      message,
+      url: github_url,
+      id: commit_sha
+    } = this.state.semaphoreResponse.commit
+
+    return (
+      <li>
+        <span className='server-name'>{this.props.data.name}</span> -
+        <a href={github_url} title={commit_sha}>{message}</a>
+      </li>
+      )
+  }
+}
+
+export default Server
