3ae91cfb0218c6e469acdd524f6f1059c2a4deae
Garrett Motzner <garrett.motzner@welltok.com>
2016-02-01 14:20:30 -0700

    Get build branch with server status

--- src/popup/serverCompn.js
+++ src/popup/serverCompn.js
@@ -1,36 +1,52 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import ReactDOM from 'react-dom'
 import http from 'axios'
 
 class Server extends React.Component {
-  constructor(props) {
-    super(props);
+  static propTypes = {
+    data: PropTypes.object.isRequired
+  }
+
+  constructor (props) {
+    super(props)
     this.displayName = 'ServerStatusDetail'
-    this.state = {semaphoreResponse: {commit:{}}}
+    this.state = {
+      serverStatusData: { commit: {} },
+      buildStatusData: {}
+    }
   }
 
   componentDidMount () {
     http.get(this.props.data.server_url)
-      .then((response) => { this.setState({semaphoreResponse: response.data}) })
+      .then(({data}) => {
+        this.setState({ serverStatusData: data })
+        return http.get(data.build_url)
+      })
+      .then(({data}) => {
+        this.setState({ buildStatusData: data })
+      })
       .catch((response) => console.error('semaphore server error:', response))
   }
 
-  render() {
+  render () {
     const {
-      result: status,
-      server_html_url,
-      build_html_url,
-      commit: {
-        message,
-        url: github_url,
-        id: commit_sha
-      }
-    } = this.state.semaphoreResponse
+      serverStatusData: {
+        result: status,
+        server_html_url,
+        build_html_url,
+        commit: {
+          message,
+          url: github_url,
+          id: commit_sha
+        }
+      },
+      buildStatusData: {branch_name}
+    } = this.state
 
     return !!status && (
       <li className={status}>
         <a className='server-name' href={server_html_url} title='server page' target='_blank'>{this.props.data.name}</a> -{' '}
-        <a href={build_html_url} title='build page' target='_blank'>{message}</a>{' '}
+        <a href={build_html_url} title='build page' target='_blank'>{branch_name || '(no build found)'} - {message}</a>{' '}
         <a href={github_url} title='github repo' className='hash' target='_blank'>{(commit_sha||'').substr(0,7)}</a>
       </li>
       )
